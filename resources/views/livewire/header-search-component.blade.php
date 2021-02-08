edc7cbe66222c8a9eecbaf36f65ed4f47723fdfe
unknown <abiodun_flb@yahoo.com>
2021-02-08 22:13:54 +0100

    added search product component

---
+++ resources/views/livewire/header-search-component.blade.php
@@ -0,0 +1,19 @@
+                        <div class="wrap-search center-section">
+							<div class="wrap-search-form">
+								<form action="#" id="form-search-top" name="form-search-top">
+									<input type="text" name="search" value="" placeholder="Search here...">
+									<button form="form-search-top" type="button"><i class="fa fa-search" aria-hidden="true"></i></button>
+									<div class="wrap-list-cate">
+										<input type="hidden" name="product-cate" value="0" id="product-cate">
+										<a href="#" class="link-control">All Category</a>
+										<ul class="list-cate">
+                                            <li class="level-0">All Category</li>
+                                            @foreach($catgories as $category)
+
+                                            <li class="level-1">-{{$category->name}}</li>
+                                            @endforeach
+										</ul>
+									</div>
+								</form>
+							</div>
+						</div>
